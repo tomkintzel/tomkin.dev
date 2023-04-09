95cfbf71c312431077156d19552bf1781ca5b65f
tki <kintzel@ecommerce-werkstatt.de>
2023-04-09 21:04:38 +0200

    chore: rm unecc. files

--- components.d.ts
+++ components.d.ts
@@ -9,6 +9,8 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    Icon: typeof import('./src/components/Icon.vue')['default']
+    Navbar: typeof import('./src/components/Navbar.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     TheCounter: typeof import('./src/components/TheCounter.vue')['default']
